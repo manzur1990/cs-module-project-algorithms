039c73a49e3dba5c6ce826835f2b7aadb0fda4b1
JM <manzur-1990@hotmail.com>
2020-09-17 16:21:02 -0500

    Day 3 MVP

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,41 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
 
-    pass
+
+def eating_cookies(n, cache=None):
+    print(n)
+    # Your code here
+    # base case:
+    # this represents there being a number of cookies were we can just take
+    #that many cookies
+    if n < 0:
+        return 0
+    elif n == 0:
+        return 1
+    # check the cache to see if the asnwer is stored in there
+    elif cache and cache[n] > 0:
+        return cache[n]
+    else:
+        if not cache:
+            #init an empty cache
+            cache = {i: 0 for i in range(n + 1)}
+    # store answers in our cache
+    cache[n] = eating_cookies(n - 1, cache) + eating_cookies(
+        n - 2, cache) + eating_cookies(n - 3, cache)
+
+    return cache[n]
+
+
+# this implementation does not efficiently scales because of its branching nature,
+# the run time is O(3^)
+
+# print(eating_cookies(20))
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 5
+    num_cookies = 1
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(
+        f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies"
+    )
--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,15 +2,45 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
+
+
 def sliding_window_max(nums, k):
     # Your code here
+    # k is window
+    # make a new array to hold temp numbers in window (if k=3, hold array index0, index1, index2)
+    # make another array to hold expected output
+    # loop (first pass is 1, so ind0, ind1, ind2. 2nd pass is 2, so ind1, ind2, ind3)
+    # loop over numbers in window
+    # append numbers to window_array
+    # sort window array
+    # get the last element of the window array (max)
+    # append max to expected_output array
+    # delete first number of the nums array, so you start on the next window
+    # clear the window_array to start fresh on the next loop
+    # return expected output array
 
-    pass
+    window_array = []
+    expected_output = []
+    while 0 < len(nums) - (k - 1):
+        for num in nums[0:k]:
+            window_array.append(num)
+        # sort window_array in ascending order
+        window_array.sort()
+        # assign variable max_number to the last elemetn on window_array
+        max_number = window_array[k - 1]
+        # add max_number to expected_output array
+        expected_output.append(max_number)
+        # pop the rest of the elements that fall into the first index in a sequence
+        nums.pop(0)
+        window_array = []
+    return expected_output
 
 
 if __name__ == '__main__':
-    # Use the main function here to test out your implementation 
+    # Use the main function here to test out your implementation
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
 
-    print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
+    print(
+        f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}"
+    )
